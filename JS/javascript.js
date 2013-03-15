b11033b0251dd3c0ac2de54ab174f0de1cfe381c
cmshipman <caitlin.shipman@verizon.net>
2013-03-14 23:33:10 -0400

    gave functions unique names

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,47 +1,54 @@
 //validates the input for street address 1
 function validateForm()
 {
-var x=document.forms["input"]["street1"].value;
-if (x==null || x=="")
-	{
-	alert("Street Address 1 must be filled out!");
-	return false;
-	}
+	validateAddress();
+	validateCity();
+	validateState();
+	validateEmail();
+}
+function validateAddress()
+{
+	var x=document.forms["input"]["street1"].value;
+	if (x==null || x=="")
+		{
+		alert("Street Address 1 must be filled out!");
+		return false;
+		}
 }
 //made with help from Reyna Dawkins! (:
 
 //validates the input for city
-function validateForm()
+function validateCity()
 {
-var x=document.forms["input"]["city"].value;
-if (x==null || x=="")
-	{	
-	alert("City must be filled out!");
-	return false;
-	}
+	var x=document.forms["input"]["city"].value;
+	if (x==null || x=="")
+		{	
+		alert("City must be filled out!");
+		return false;
+		}
 }
 
 //validates the input for state
-function validateForm()
+function validateState()
 {
-var x=document.forms["myForm"]["state"]
-if (x==null || x==""
-	{
-	alert("Please select a state!");
-	return false;
-	}
+	var x=document.forms["myForm"]["state"]
+	if (x==null || x==""
+		{
+		alert("Please select a state!");
+		return false;
+		}
 }
 
 //validates the input for email
-function validateForm()
+function validateEmail()
 {
-var x=document.forms["myForm"]["email"].value;
-var atpos=x.indexOf("@");
-var dotpos=x.lastIndexOf(".");
-if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
-	{
-	a;er("Not a valid e-mail address");
-	}
+	var x=document.forms["myForm"]["email"].value;
+	var atpos=x.indexOf("@");
+	var dotpos=x.lastIndexOf(".");
+	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+		{
+		alert("Not a valid e-mail address");
+		}
 }
 
 		
